be775a605cd32a894b8559b08eae133e12da7bf0
habaumugisha1 <habajeunes2@gmail.com>
2020-08-26 10:02:53 +0200

    adding confirm boxes to new article

--- UI/newArticle.js
+++ UI/newArticle.js
@@ -1,4 +1,6 @@
 const inputs = document.querySelectorAll('.focused');
+const btn = document.getElementById('btnart')
+const artform = document.getElementById('articleForm')
 
 inputs.forEach(input => {
     input.addEventListener('focus', focusFunction);
@@ -15,3 +17,14 @@ function blurFunction(){
         parent.classList.remove('focus');
     }
 }
+btn.addEventListener('click', () => {
+    const confirmArt = confirm("Do you want to publish?");
+        if( confirmArt == true ) {
+                alert("Article published");
+                artform.reset()
+                return true;
+        } else {
+             alert("Article not published")
+                 return false;
+                   }
+});
